a34b7ae04f3009a98d293c6539bf3816eb1474a9
cjlester <ChrisJ.Lester@ymail.com>
2016-05-18 20:56:59 +0100

    multi bt msgs sent

--- app/src/main/java/com/beaumont/chrisj/bc_solodualcontrols/MainActivity.java
+++ app/src/main/java/com/beaumont/chrisj/bc_solodualcontrols/MainActivity.java
@@ -8,6 +8,8 @@ import android.content.ClipDescription;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,7 +21,9 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
+import android.widget.TableLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,11 +33,15 @@ public class MainActivity extends AppCompatActivity {
 
     BluetoothAdapter mBluetoothAdapter;
     BluetoothDevice mDevice;
+    BluetoothChatService mChatService;
 
     private final static int REQUEST_ENABLE_BT = 1;
 
+    boolean[] deviceA = {false, false, false, false};
+    boolean[] deviceB = {false, false, false, false};
+
     String msg;
-    int draggedlbl;
+    int dragged_lbl;
 
     Boolean flag;
 
@@ -112,12 +120,94 @@ public class MainActivity extends AppCompatActivity {
                 for (BluetoothDevice device : pairedDevices) {
                     if (device.getName().equals(mArrayAdapter.getItem(position).toString())) {
                         mDevice = device;
+                        ((TextView) findViewById(R.id.lblOtherDevice)).setText("Other device: " + mDevice.getName());
+                        initChatService();
                         break;
                     }
                 }
                 alert.cancel();
             }
         });
+    }
+
+    private void initChatService(){
+        mChatService = new BluetoothChatService(getApplicationContext(), new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                switch (msg.what) {
+                    case Constants.MESSAGE_STATE_CHANGE:
+                        break;
+                    case Constants.MESSAGE_WRITE:
+                        break;
+                    case Constants.MESSAGE_READ:
+                        // construct a string from the valid bytes in the buffer
+                        parseMsg(new String((byte[]) msg.obj, 0, msg.arg1));
+                        break;
+                    case Constants.MESSAGE_DEVICE_NAME:
+                        break;
+                    case Constants.MESSAGE_TOAST:
+                        break;
+                }
+            }
+        });
+        mChatService.connect(mDevice, true);
+    }
+
+    private void parseMsg(String s){
+        int code = Integer.parseInt(s);
+
+        switch (code) {
+            case (999):
+                reset();
+                break;
+            case (101):
+                findViewById(R.id.lblLaunchCons).setVisibility(TextView.INVISIBLE);
+                findViewById(R.id.lc_2).setVisibility(TextView.VISIBLE);
+                break;
+            case (102):
+                findViewById(R.id.lblStreamCons).setVisibility(TextView.INVISIBLE);
+                findViewById(R.id.lblFlightCons).setVisibility(TextView.INVISIBLE);
+                findViewById(R.id.sc_2).setVisibility(TextView.VISIBLE);
+                findViewById(R.id.fc_1).setVisibility(TextView.VISIBLE);
+                break;
+            case (103):
+                findViewById(R.id.lblFlightCons).setVisibility(TextView.INVISIBLE);
+                findViewById(R.id.lblStreamCons).setVisibility(TextView.INVISIBLE);
+                findViewById(R.id.fc_2).setVisibility(TextView.VISIBLE);
+                findViewById(R.id.sc_1).setVisibility(TextView.VISIBLE);
+                break;
+            case (104):
+                findViewById(R.id.lblLandCons).setVisibility(TextView.INVISIBLE);
+                findViewById(R.id.lsc_2).setVisibility(TextView.VISIBLE);
+                break;
+
+            case (201):
+                findViewById(R.id.lblLaunchCons).setVisibility(TextView.INVISIBLE);
+                findViewById(R.id.lc_1).setVisibility(TextView.VISIBLE);
+                break;
+            case (202):
+                findViewById(R.id.lblStreamCons).setVisibility(TextView.INVISIBLE);
+                findViewById(R.id.lblFlightCons).setVisibility(TextView.INVISIBLE);
+                findViewById(R.id.sc_1).setVisibility(TextView.VISIBLE);
+                findViewById(R.id.fc_2).setVisibility(TextView.VISIBLE);
+                break;
+            case (203):
+                findViewById(R.id.lblFlightCons).setVisibility(TextView.INVISIBLE);
+                findViewById(R.id.lblStreamCons).setVisibility(TextView.INVISIBLE);
+                findViewById(R.id.fc_1).setVisibility(TextView.VISIBLE);
+                findViewById(R.id.sc_2).setVisibility(TextView.VISIBLE);
+                break;
+            case (204):
+                findViewById(R.id.lblLandCons).setVisibility(TextView.INVISIBLE);
+                findViewById(R.id.lsc_1).setVisibility(TextView.VISIBLE);
+                break;
+            default:
+                break;
+        }
+
+        Button btnReset = (Button) findViewById(R.id.btnRestart);
+        if((code < 999) && (btnReset.getVisibility() == Button.INVISIBLE))
+            btnReset.setVisibility(Button.VISIBLE);
     }
 
     public void flight_controls(View view){
@@ -160,43 +250,65 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public boolean onDrag(View v, DragEvent event) {
-            switch (event.getAction()) {
-                case DragEvent.ACTION_DRAG_STARTED:
-                    Log.d(msg, "Action is DragEvent.ACTION_DRAG_STARTED");
-                    break;
-                case DragEvent.ACTION_DRAG_ENTERED:
-                    Log.d(msg, "Action is DragEvent.ACTION_DRAG_ENTERED");
 
-                    if(v.getId() == R.id.frame_flight){
-                        v.setBackgroundColor(Color.BLUE);
-                        flag = true;
-                    } else if (v.getId() == R.id.frame_stream){
-                        v.setBackgroundColor(Color.BLUE);
-                        flag = false;
-                    }
+            boolean result = false;
+
+            switch (dragged_lbl){
+                case R.id.lblLaunchCons:
+                    result = parseDragEvent(v, event);
                     break;
-                case DragEvent.ACTION_DRAG_EXITED:
-                    Log.d(msg, "Action is DragEvent.ACTION_DRAG_EXITED");
-                    if(v.getId() == R.id.frame_flight || v.getId() == R.id.frame_stream)
-                        v.setBackgroundColor(Color.TRANSPARENT);
+                case R.id.lblStreamCons:
+                    result = parseDragEvent(v, event);
                     break;
-                case DragEvent.ACTION_DRAG_LOCATION:
-                    Log.d(msg, "Action is DragEvent.ACTION_DRAG_LOCATION");
+                case R.id.lblFlightCons:
+                    result = parseDragEvent(v, event);
                     break;
-                case DragEvent.ACTION_DRAG_ENDED:
-                    Log.d(msg, "Action is DragEvent.ACTION_DRAG_ENDED");
-                    findViewById(R.id.frame_flight).setBackgroundColor(Color.TRANSPARENT);
-                    findViewById(R.id.frame_stream).setBackgroundColor(Color.TRANSPARENT);
-                    moveControl();
-                    break;
-                case DragEvent.ACTION_DROP:
-                    Log.d(msg, "ACTION_DROP event");
-                    break;
-                default:
-                    break;
+                case R.id.lblLandCons:
+                    result = parseDragEvent(v, event);
             }
-            return true;
+
+            return result;
         }
+    }
+
+    private boolean parseDragEvent(View v, DragEvent event){
+        switch (event.getAction()) {
+            case DragEvent.ACTION_DRAG_STARTED:
+                Log.d(msg, "Action is DragEvent.ACTION_DRAG_STARTED");
+                break;
+            case DragEvent.ACTION_DRAG_ENTERED:
+                Log.d(msg, "Action is DragEvent.ACTION_DRAG_ENTERED");
+
+                if(v.getId() == R.id.frame_flight){
+                    v.setBackgroundColor(Color.BLUE);
+                    flag = true;
+                } else if (v.getId() == R.id.frame_stream){
+                    v.setBackgroundColor(Color.BLUE);
+                    flag = false;
+                }
+                break;
+            case DragEvent.ACTION_DRAG_EXITED:
+                Log.d(msg, "Action is DragEvent.ACTION_DRAG_EXITED");
+                if(v.getId() == R.id.frame_flight || v.getId() == R.id.frame_stream)
+                    v.setBackgroundColor(Color.TRANSPARENT);
+                break;
+            case DragEvent.ACTION_DRAG_LOCATION:
+                Log.d(msg, "Action is DragEvent.ACTION_DRAG_LOCATION");
+                break;
+            case DragEvent.ACTION_DRAG_ENDED:
+                Log.d(msg, "Action is DragEvent.ACTION_DRAG_ENDED");
+                findViewById(R.id.frame_flight).setBackgroundColor(Color.TRANSPARENT);
+                findViewById(R.id.frame_stream).setBackgroundColor(Color.TRANSPARENT);
+                moveToList();
+                makeToast("Ended");
+                break;
+            case DragEvent.ACTION_DROP:
+                Log.d(msg, "ACTION_DROP event");
+                break;
+            default:
+                break;
+        }
+        return true;
     }
 
     public class mOnTouchListener implements View.OnTouchListener{
@@ -208,7 +320,7 @@ public class MainActivity extends AppCompatActivity {
 
                 v.startDrag(data, shadowBuilder, v, 0);
                 v.setVisibility(View.INVISIBLE);
-                draggedlbl = v.getId();
+                dragged_lbl = v.getId();
                 return true;
             }
             else
@@ -218,49 +330,69 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void moveControl(){
+    private void moveToList(){
         Button btnRestart = (Button)findViewById(R.id.btnRestart);
         if(btnRestart.getVisibility() == Button.INVISIBLE)
             btnRestart.setVisibility(Button.VISIBLE);
 
         if(!flag){
-            switch (draggedlbl){
+            switch (dragged_lbl){
                 case R.id.lblLaunchCons:
                     findViewById(R.id.lc_1).setVisibility(TextView.VISIBLE);
+                    deviceA[0] = true;
+                    sendBT("101");
                     break;
                 case R.id.lblStreamCons:
                     findViewById(R.id.sc_1).setVisibility(TextView.VISIBLE);
                     findViewById(R.id.fc_2).setVisibility(TextView.VISIBLE);
                     findViewById(R.id.lblFlightCons).setVisibility(TextView.INVISIBLE);
+                    deviceA[1] = true;
+                    deviceB[2] = true;
+                    sendBT("102");
                     break;
                 case R.id.lblFlightCons:
                     findViewById(R.id.fc_1).setVisibility(TextView.VISIBLE);
                     findViewById(R.id.sc_2).setVisibility(TextView.VISIBLE);
                     findViewById(R.id.lblStreamCons).setVisibility(TextView.INVISIBLE);
+                    deviceA[2] = true;
+                    deviceB[1] = true;
+                    sendBT("103");
                     break;
                 case R.id.lblLandCons:
                     findViewById(R.id.lsc_1).setVisibility(TextView.VISIBLE);
+                    deviceA[3] = true;
+                    sendBT("104");
                     break;
                 default:
                     break;
             }
         } else {
-            switch (draggedlbl){
+            switch (dragged_lbl){
                 case R.id.lblLaunchCons:
                     findViewById(R.id.lc_2).setVisibility(TextView.VISIBLE);
+                    deviceB[0] = true;
+                    sendBT("201");
                     break;
                 case R.id.lblStreamCons:
                     findViewById(R.id.sc_2).setVisibility(TextView.VISIBLE);
                     findViewById(R.id.fc_1).setVisibility(TextView.VISIBLE);
                     findViewById(R.id.lblFlightCons).setVisibility(TextView.INVISIBLE);
+                    deviceB[1] = true;
+                    deviceA[2] = true;
+                    sendBT("202");
                     break;
                 case R.id.lblFlightCons:
                     findViewById(R.id.fc_2).setVisibility(TextView.VISIBLE);
                     findViewById(R.id.sc_1).setVisibility(TextView.VISIBLE);
                     findViewById(R.id.lblStreamCons).setVisibility(TextView.INVISIBLE);
+                    deviceB[2] = true;
+                    deviceA[1] = true;
+                    sendBT("203");
                     break;
                 case R.id.lblLandCons:
                     findViewById(R.id.lsc_2).setVisibility(TextView.VISIBLE);
+                    deviceB[3] = true;
+                    sendBT("204");
                     break;
                 default:
                     break;
@@ -271,12 +403,18 @@ public class MainActivity extends AppCompatActivity {
                 findViewById(R.id.lblStreamCons).getVisibility() == TextView.INVISIBLE &&
                 findViewById(R.id.lblFlightCons).getVisibility() == TextView.INVISIBLE &&
                 findViewById(R.id.lblLandCons).getVisibility() == TextView.INVISIBLE){
+
             findViewById(R.id.btnStart).setVisibility(Button.VISIBLE);
         }
 
     }
 
-    public void onBtnRestart(View v){
+    public void onBtnReset(View v){
+        sendBT("999");
+        reset();
+    }
+
+    private void reset(){
         findViewById(R.id.btnRestart).setVisibility(Button.INVISIBLE);
         findViewById(R.id.btnStart).setVisibility(Button.INVISIBLE);
 
@@ -294,6 +432,19 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.fc_2).setVisibility(TextView.GONE);
         findViewById(R.id.sc_2).setVisibility(TextView.GONE);
         findViewById(R.id.lsc_2).setVisibility(TextView.GONE);
+
+        for(int i = 0; i < 4; i++) {
+            deviceA[i] = false;
+            deviceB[i] = false;
+        }
+    }
+
+    public void onBtnStart(View v){
+
+    }
+
+    private void sendBT(String msg){
+        mChatService.write(msg.getBytes());
     }
 
     private void makeToast(String m){
